946163a289fc60c54e3f137b2e4e4f80d745e407
sp381 <sarah.powell381@gmail.com>
2022-05-03 12:50:38 -0700

    first commit / product.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
@@ -8,7 +8,7 @@ const sequelize = process.env.JAWSDB_URL
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
-        decimalNumbers: true,
+      decimalNumbers: true,
       },
     });
 
